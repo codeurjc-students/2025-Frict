231d05ef180a685893ee482c2a6cb9ff522eeb2c
Marcos Jiménez <m.jimenezp.2021@alumnos.urjc.es>
2025-07-06 21:14:05 +0200

    Angular routing configured

--- ProjectFrontend/src/app/app.component.ts
+++ ProjectFrontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router'; //Es el componente en el que se integra el contenido del router, e importa RouterLink por ser usado en los botones de nav
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ProjectFrontend/src/app/app.routes.ts
+++ ProjectFrontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {IndexComponent} from './index/index.component';
+import {ProductInfoComponent} from './product-info/product-info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'index', component: IndexComponent}, //La ruta '' será la que se muestre por defecto en el router cuando se inicia la aplicación. Si no se define, el router no mostrará nada dentro de su componente
+  {path: 'product-info', component: ProductInfoComponent}
+];
