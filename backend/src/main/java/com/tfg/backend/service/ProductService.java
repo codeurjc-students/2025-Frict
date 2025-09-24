ad85eccc9bcce473fece4d73575079e0303554d4
Marcos Jiménez <m.jimenezp.2021@alumnos.urjc.es>
2025-09-24 11:49:00 +0200

    Server side unit tests

--- backend/src/main/java/com/tfg/backend/service/ProductService.java
+++ backend/src/main/java/com/tfg/backend/service/ProductService.java
@@ -3,7 +3,9 @@ package com.tfg.backend.service;
 import com.tfg.backend.model.Product;
 import com.tfg.backend.repository.OrderRepository;
 import com.tfg.backend.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,24 +15,47 @@ import java.util.Optional;
 public class ProductService {
 
     @Autowired
-    private ProductRepository repository;
+    private ProductRepository productRepository;
 
     @Autowired
     private OrderRepository orderRepository;
 
     public List<Product> findAll() {
-        return repository.findAll();
-    }
-
-    public Product save(Product p) {
-        return repository.save(p);
+        return productRepository.findAll();
     }
 
     public Optional<Product> findById(Long id) {
-        return repository.findById(id);
+        return productRepository.findById(id);
+    }
+
+    //Check that the reference code is not being used yet and all product fields are valid
+    public Product save(Product p) {
+        //Comprueba que el número de referencia es único
+        if(productRepository.existsByReferenceCode(p.getReferenceCode())){
+            throw new IllegalArgumentException("The reference code is already taken");
+        }
+        else if (p.getName() == null || p.getName().isEmpty()){
+            throw new IllegalArgumentException("The title is null or empty");
+        }
+        else if (p.getPrice() < 0){
+            throw new IllegalArgumentException("The price should be positive or 0");
+        }
+        else return productRepository.save(p);
+    }
+
+    public Product update(Product product) {
+        Optional<Product> productOpt = productRepository.findById(product.getId());
+        if (!productOpt.isPresent()) {
+            throw new EntityNotFoundException("The product that is being updated does not exist");
+        }
+        return this.save(product);
     }
 
     public void deleteById(Long id) {
-        repository.deleteById(id);
+        Optional<Product> productOpt = productRepository.findById(id);
+        if (!productOpt.isPresent()) {
+            throw new EntityNotFoundException("The product that is being deleted does not exist");
+        }
+        productRepository.deleteById(id);
     }
 }
---
+++ backend/src/main/java/com/tfg/backend/utils/ServiceErrorHandler.java
@@ -0,0 +1,35 @@
+package com.tfg.backend.utils;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/*
+Handles the format exceptions thrown up from the services to the controllers:
+
+EntityNotFoundException: An object could not be found in database
+IllegalArgumentException: The object that is saving in the database does not have the right fields or format
+MethodArgumentNotValidException:
+
+ */
+
+@ControllerAdvice(basePackages = "com.tfg.backend.controller")
+public class ServiceErrorHandler {
+
+    @ExceptionHandler({ EntityNotFoundException.class, IllegalArgumentException.class, MethodArgumentNotValidException.class })
+    public ResponseEntity<?> handleException(Exception ex) {
+        return switch (ex) {
+            case MethodArgumentNotValidException manvExp -> {
+                var fieldError = manvExp.getFieldError();
+                yield ResponseEntity.badRequest().body(fieldError != null ? fieldError.getDefaultMessage() : "Validation error");
+            }
+            case EntityNotFoundException enfExp -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(enfExp.getMessage());
+            default -> ResponseEntity.badRequest().body(ex.getMessage());
+        };
+    }
+
+}
